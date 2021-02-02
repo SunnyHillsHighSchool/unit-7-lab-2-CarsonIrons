174ebec1fe8352cdbf107d9539c9a4e0abb167ba
Carson Irons <600020180@fjuhsd.org>
2021-02-02 17:50:08 +0000

    creates go method and added testing to main

--- ListLast.java
+++ ListLast.java
@@ -8,8 +8,25 @@ import java.util.ArrayList;
 
 public class ListLast
 {
+   // Method for checking for a repeat final value
+   //By Carson Irons P2
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
-	}
+//First check if the arraylist has at least 2 elements 
+if(ray.size()>=2){
+//if it does make a for loop starting at 0and ending at the second to last element
+for(int i =0; i<ray.size()-1;i++){
+//for each index check if it is equal to the final element 
+if(ray.get(i)==ray.get(ray.size()-1)){
+//if any are return true
+return true;
+}
+//end for loop
+}
+//end if statement
+}
+//if true is not returned already return false
+return false;
+//end method
+  }
 }
